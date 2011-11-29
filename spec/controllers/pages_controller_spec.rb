9d60bab8c212ea02154eaf4091c3bd6dfc1c83f1
ChibiGato <Chelsea@.(none)>
2011-11-28 20:53:52 -0600

    Added titles and tests for them

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,6 +7,11 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Home")
+    end
   end
 
   describe "GET 'contact'" do
@@ -14,12 +19,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content=>"Contact")
+    end
   end
 
   describe "GET 'about'" do
     it "returns http success" do
       get 'about'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content=>"About")
     end
   end
 
