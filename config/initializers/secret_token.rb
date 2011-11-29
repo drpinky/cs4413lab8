47475841f0223b2fb9ad249309b210d4159c7835
ChibiGato <Chelsea@.(none)>
2011-11-28 20:18:58 -0600

    Initial commit and added rspec install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab8::Application.config.secret_token = '4b7100578ce078ef88c118d7c4148fc722b4e7f19c6fe49d004eb8a0d53ceffc2dee3695910fa283012139faa562746e892e8145467082475730e9b6ab7cb9c4'
